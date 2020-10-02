b1d3c96a171098654bea3b6a88071dd817cbfa99
Ethan Dickey <etdickey@gmail.com>
2020-10-01 23:17:34 -0500

    Added ValidationExceptionTest to the right package.

---
+++ Prog0/src/sdns/serialization/test/ValidationExceptionTest.java
@@ -0,0 +1,31 @@
+package sdns.serialization.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import sdns.serialization.ValidationException;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ValidationExceptionTest {
+    /**
+     * Constructor tests (and, by extension, getBadToken test
+     */
+    @Nested
+    class constructorTests {
+        @Test @DisplayName("Basic valid test")
+        void testConst(){
+            ValidationException ve = new ValidationException("hi", "null pointer");
+            assertAll("Members", () -> assertEquals("hi", ve.getMessage()),
+                        () -> assertEquals("null pointer", ve.getBadToken()));
+        }
+
+        @Test @DisplayName("Throwable cause")
+        void testConstThrowable(){
+            Throwable t = new ValidationException("Hehe nested validation exception", "/shrug");
+            ValidationException ve = new ValidationException("hi", t, "null pointer");
+            assertAll("Throwable", () -> assertEquals(ValidationException.class, ve.getCause().getClass()),
+                    () -> assertEquals("Hehe nested validation exception", ve.getCause().getMessage()),
+                    () -> assertEquals("/shrug", ((ValidationException)ve.getCause()).getBadToken()));
+        }
+    }
+}
